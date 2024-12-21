b38f344f36ca31c99e32af53b5ef2d5b775b5d98
Ronald Villagrán <ronald3217@gmail.com>
2024-12-20 18:00:44 -0600

    🗑 feat(testing.js): remove unnecessary testing file
    BREAKING CHANGE: testing.js file has been deleted, any dependencies on this file will need to be updated.
    
    Deletes testing.js file containing react and react-dom/server imports, and removes the renderedIconSportAllSports logging.

--- testing.js
+++
@@ -1,7 +0,0 @@
-// testing.js
-import React from 'react';
-import ReactDOMServer from 'react-dom/server';
-import IconSportAllSports from './lib/sports/solid/icon-sport-all-sports.jsx';
-
-const renderedIconSportAllSports = ReactDOMServer.renderToString(<IconSportAllSports />);
-console.log(renderedIconSportAllSports);
